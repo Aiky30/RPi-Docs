1666bae8bc1ed6ed5ad8ba673357211c58975d9c
ChangoFoster <samueljamesfoster@gmail.com>
2019-08-07 16:43:15 +0100

    Display the temp as a chart and sve timestamp

--- scripts/pi_temp.py
+++ scripts/pi_temp.py
@@ -1,12 +1,33 @@
 import os
 import time
+from datetime import datetime
 
+x_axis = []
+y_axis = []
+graph = []
 
 def get_cpu_temp():
-        cpu_temp = os.popen("vcgencmd measure_temp").readline()
-        return cpu_temp.replace("temp=","")
+    cpu_temp = os.popen("vcgencmd measure_temp").readline()
+    #Convert the temp read from the OS to a clean float
+    return float(cpu_temp.replace("temp=","").replace("'C\n", ""))
 
-
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S") # 24-Hour:Minute:Second
+    return  current_time
+                        
 while True:
-        print(get_cpu_temp())
-        time.sleep(.5)
+    #Only keep the latest 5 inputs
+    if len(x_axis) == 5 and len(y_axis) == 5:
+        #Ignore the room temp
+        multiplier = int(y_axis[0]) - 30
+        print("|" * multiplier)
+        del x_axis[0]
+        del y_axis[0]
+    else:
+        print("Getting data...")
+    #Add latest date
+    x_axis.append(get_time())
+    y_axis.append(get_cpu_temp())
+    time.sleep(0.5)
+        
