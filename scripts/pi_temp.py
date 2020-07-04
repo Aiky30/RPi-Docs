7e7429f0f024e1fee21bb098038101e983080468
ChangoFoster <samueljamesfoster@gmail.com>
2020-07-04 18:10:46 +0100

    Fixed comments from PR

--- scripts/pi_temp.py
+++ scripts/pi_temp.py
@@ -8,7 +8,7 @@ y_axis = []
 
 def get_cpu_temp():
     cpu_temp = os.popen("vcgencmd measure_temp").readline()
-    #Convert the temp read from the OS to a clean float
+    # Convert the temp read from the OS to a clean float
     return float(cpu_temp.replace("temp=","").replace("'C\n", ""))
 
 def get_time():
@@ -16,15 +16,14 @@ def get_time():
     current_time = now.strftime("%H:%M:%S") # 24-Hour:Minute:Second
     return  current_time
                         
-while True:
-    #Add latest date
-    x_axis.append(get_time())
-    y_axis.append(get_cpu_temp())
-    #Create elements for graph
-    latest_time = str(x_axis[-1]) + " " 
-    latest_temp = str(y_axis[-1]) + " "
-    temp_lines = str("|" * (int(y_axis[-1]) - 30))
-    #Print line for graph every .5 seconds
-    print(latest_time + latest_temp + temp_lines)
+while True:    
+    temperature = get_cpu_temp()
+    bar_size = int(temperature) - 30
+    bar = str("|" * bar_size) 
+    
+    print("{time} {temp} {graph}".format(
+        time=get_time(), temp=temperature, graph=bar
+    ))
+    
     time.sleep(0.5)
         
