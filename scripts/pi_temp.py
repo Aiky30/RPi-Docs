20169c77080d380cd6f8c6f5c40747033f7452ef
Sam <samueljamesfoster@gmail.com>
2020-07-10 10:05:04 +0100

    Adding in timestamp and bar chart to temp monitor (#2)
    * Display the temp as a chart and sve timestamp
    
    * Adding OS check

--- scripts/pi_temp.py
+++ scripts/pi_temp.py
@@ -1,12 +1,32 @@
 import os
 import time
+from datetime import datetime
 
+def check_os():
+    return os.uname()[4].startswith("arm") 
 
 def get_cpu_temp():
-        cpu_temp = os.popen("vcgencmd measure_temp").readline()
-        return cpu_temp.replace("temp=","")
+    cpu_temp = os.popen("vcgencmd measure_temp").readline()
+    
+    # Convert the temp read from the OS to a clean float
+    return float(cpu_temp.replace("temp=","").replace("'C\n", ""))
 
-
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S") # 24-Hour:Minute:Second
+    return  current_time
+                        
 while True:
-        print(get_cpu_temp())
-        time.sleep(.5)
+    if not check_os():
+        print("only rasbian os supported")
+        break
+
+    temperature = get_cpu_temp()
+    bar_size = int(temperature) - 30
+    bar = str("|" * bar_size) 
+    
+    print("{time} {temp} {graph}".format(
+        time=get_time(), temp=temperature, graph=bar
+    ))
+    
+    time.sleep(0.5)   
